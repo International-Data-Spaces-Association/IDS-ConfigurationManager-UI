6a06411387ca9878101b739d8d0f8b2513b57738
Bastian Weltjen <bastian.weltjen@fkie.fraunhofer.de>
2021-03-16 22:12:22 +0100

    chore: docker run with npm start (because env variables not working with prebuild)

--- entryPoint.sh
+++ entryPoint.sh
@@ -6,7 +6,4 @@ sed -i "s@#UI_BACKEND_URL#@$UI_BACKEND_URL@g" .env
 sed -i "s@#UI_TITLE#@$UI_TITLE@g" .env
 sed -i "s@#SHOW_FREENOW_APP#@$SHOW_FREENOW_APP@g" .env
 
-
-
-npm run backend &
-serve -l 8082 -s dist
+npm run-script startprod
